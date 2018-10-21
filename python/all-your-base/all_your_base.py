6d880bdc27537d759b1d4a292ebb4dd1a54cb704
Kyle Rader <kyle@kylerader.ninja>
2018-10-21 13:11:40 -0700

    Refactor to be more pythonic

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,18 +1,17 @@
-def rebase(input_base, digits, output_base):
-    if output_base <= 1:
-        raise ValueError("output base must be greater than 1")
-    if input_base <= 1:
-        raise ValueError("input base must be greater than 1")
-    if len(list(filter(lambda x: x != 0, digits))) == 0:
+from typing import List
+
+
+def rebase(input_base: int, digits: List[int], output_base: int) -> List[int]:
+    if output_base <= 1 or input_base <= 1:
+        raise ValueError("bases must be greater than 1")
+    if not all(input_base > d >= 0 for d in digits):
+        raise ValueError("All digits must be greater than or equal to zero and less than the input base")
+    if not any(digits):
         return []
 
     # Build up the value in base 10 first to convert into new base
     base_10_val = 0
     for i in range(len(digits)):
-        if digits[i] < 0:
-            raise ValueError("digits must all be positive or zero")
-        if digits[i] >= input_base:
-            raise ValueError("digits must be less than the input base")
         base_10_val += digits[i] * (input_base ** (len(digits) - i - 1))
 
     new_digits = []
@@ -22,4 +21,3 @@ def rebase(input_base, digits, output_base):
         new_digits.insert(0, digit)
 
     return new_digits
-    
