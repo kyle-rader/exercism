5c6694cf2772d8f22bf4fcedb5a677c433058006
Kyle W. Rader <kyle@kylerader.ninja>
2018-10-30 17:47:45 -0700

    Implement numbers and full house

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -18,9 +18,39 @@ def score_yacht(dice):
     return 50 if all(d == dice[0] for d in dice) else 0
 
 
+def filter_number(dice, number):
+    return [d for d in dice if d == number]
+
+
+def score_number(dice, number):
+    return sum(filter_number(dice, number))
+
+
+def score_full_house(dice):
+    unique_dice = list(set(dice))
+    if len(unique_dice) != 2:
+        return 0
+
+    groups = [filter_number(dice, d) for d in unique_dice]
+    lengths = list(map(len, groups))
+    if not (2 in lengths and 3 in lengths):
+        return 0
+
+    return sum(sum(g) for g in groups)
+
+
+
+
 
 CATEGORY_FUNCS = {
-    YACHT: score_yacht
+    YACHT: score_yacht,
+    ONES: lambda d: score_number(d, 1),
+    TWOS: lambda d: score_number(d, 2),
+    THREES: lambda d: score_number(d, 3),
+    FOURS: lambda d: score_number(d, 4),
+    FIVES: lambda d: score_number(d, 5),
+    SIXES: lambda d: score_number(d, 6),
+    FULL_HOUSE: score_full_house,
 }
 
 def score(dice, category):
