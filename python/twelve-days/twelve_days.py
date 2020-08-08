429b670260e766be6754b259b9aa40c90bc394d5
Kyle Rader <kylewrader@gmail.com>
2020-08-07 21:22:54 -0700

    Twelve days of christmas

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,37 @@
+num_to_word = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+]
+
+verse = [
+    "twelve Drummers Drumming,",
+    "eleven Pipers Piping,",
+    "ten Lords-a-Leaping,",
+    "nine Ladies Dancing,",
+    "eight Maids-a-Milking,",
+    "seven Swans-a-Swimming,",
+    "six Geese-a-Laying,",
+    "five Gold Rings,",
+    "four Calling Birds,",
+    "three French Hens,",
+    "two Turtle Doves, and",
+    "a Partridge in a Pear Tree.",
+]
+
+base_verse = "On the {} day of Christmas my true love gave to me: {}"
+
+def recite(start_verse, end_verse):
+    return [gen_verse(i) for i in range(start_verse, end_verse+1)]
+
+def gen_verse(index):
+    return base_verse.format(num_to_word[index-1], " ".join(verse[12-index:]))
