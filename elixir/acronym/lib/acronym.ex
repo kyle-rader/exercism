f6677417ff9ae8bf16f6d55e473e306790f9f83d
Kyle Rader <kylewrader@gmail.com>
2024-02-19 17:14:47 -0600

    need for speed

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -5,5 +5,10 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split(~r/\W+/)
+    |> Enum.map(&String.upcase(&1))
+    |> Enum.map(&String.slice(&1, 0))
+    |> Enum.join()
   end
 end
